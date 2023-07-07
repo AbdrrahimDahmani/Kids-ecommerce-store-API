aebf3b2326f0deedff096720a97bea10e8f61f61
Abderrahim Dahmani <80603216+AbdrrahimDahmani@users.noreply.github.com>
2023-07-07 15:33:59 +0100

    added tags API

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { TagsService } from './tags.service';
+import { Tag } from 'src/entities';
+import { TagDto } from 'src/dtos/tagDto/create-tag.dto';
+import { UpdateTagDto } from 'src/dtos/tagDto/update-tag.dto';
 
 @Controller('tags')
 export class TagsController {
@@ -6,4 +19,29 @@ export class TagsController {
    *
    */
   constructor(private tagsService: TagsService) {}
+
+  @Get('')
+  getAllTags(@Query('nom') nom: string) {
+    return this.tagsService.getAllTags(nom);
+  }
+
+  @Get('/:id')
+  getTagbyId(@Param('id') id: number) {
+    return this.tagsService.getTagById(id);
+  }
+
+  @Post('')
+  createTag(@Body() tag: TagDto): Promise<Tag> {
+    return this.tagsService.createTag(tag);
+  }
+
+  @Patch('/:id')
+  updateTag(@Param('id') id: number, @Body() tag: UpdateTagDto): Promise<Tag> {
+    return this.tagsService.updateTag(id, tag);
+  }
+
+  @Delete('/:id')
+  deleteTag(@Param('id') id: number) {
+    return this.tagsService.deleteTag(id);
+  }
 }
