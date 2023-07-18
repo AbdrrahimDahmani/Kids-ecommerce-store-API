ccf08532bef741ab96cfb3668ab4bc9135910f03
Abderrahim Dahmani <80603216+AbdrrahimDahmani@users.noreply.github.com>
2023-07-18 13:48:41 +0100

    added create update delete commande

--- src/commande/commande.module.ts
+++ src/commande/commande.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CommandeService } from './commande.service';
 import { CommandeController } from './commande.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Commande } from 'src/entities';
+import { CommandeRepository } from 'src/repositories';
 
 @Module({
-  providers: [CommandeService],
-  controllers: [CommandeController]
+  imports: [TypeOrmModule.forFeature([Commande])],
+  providers: [CommandeService, CommandeRepository],
+  controllers: [CommandeController],
 })
 export class CommandeModule {}
